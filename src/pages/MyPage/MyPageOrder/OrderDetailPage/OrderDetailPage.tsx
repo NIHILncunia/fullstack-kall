bb2968f37d47be4af6702c58d91977649a8ff648
NIHILncunia <nihil_ncunia@naver.com>
2023-03-14 14:35:58 +0900

    2023 0314 문의 수정중

--- src/pages/MyPage/MyPageOrder/OrderDetailPage/OrderDetailPage.tsx
+++ src/pages/MyPage/MyPageOrder/OrderDetailPage/OrderDetailPage.tsx
@@ -28,31 +28,31 @@ export function OrderDetailPage() {
 
           <div className='order-detail-table' css={orderDetailInfoTableStyle}>
             <div className='table-header'>주문번호</div>
-            <div className='table-content'>{order.orderId}</div>
+            <div className='table-content'>{order?.orderId}</div>
             <div className='table-header'>주문자 아이디</div>
-            <div className='table-content'>{order.userDTO.userId}</div>
+            <div className='table-content'>{order?.userDTO.userId}</div>
             <div className='table-header'>수령자 이름</div>
-            <div className='table-content'>{order.name}</div>
+            <div className='table-content'>{order?.name}</div>
             <div className='table-header'>수령자 연락처</div>
-            <div className='table-content'>{order.phone_nb}</div>
+            <div className='table-content'>{order?.phone_nb}</div>
             <div className='table-header'>배송지</div>
             <div className='table-content'>
-              [{order.zip_code}]
-              {order.address_1}
-              {order.address_2}
+              [{order?.zip_code}]
+              {order?.address_1}
+              {order?.address_2}
             </div>
             <div className='table-header'>주문방법</div>
-            <div className='table-content'>{payString[order.payment]}</div>
+            <div className='table-content'>{payString[order?.payment]}</div>
             <div className='table-header'>사용 마일리지</div>
-            <div className='table-content'>{order.mileage}</div>
+            <div className='table-content'>{order?.mileage}</div>
             <div className='table-header'>주문 금액</div>
-            <div className='table-content'>{order.price?.toLocaleString()}원</div>
+            <div className='table-content'>{order?.price?.toLocaleString()}원</div>
             <div className='table-header'>고객 요청사항</div>
-            <div className='table-content'>{order.request}</div>
+            <div className='table-content'>{order?.request}</div>
             <div className='table-header'>주문상태</div>
-            <div className='table-content'>{order.order_status}</div>
+            <div className='table-content'>{order?.order_status}</div>
             <div className='table-header'>주문일자</div>
-            <div className='table-content'>{moment(order.date).format('YYYY-MM-DD HH:mm:ss')}</div>
+            <div className='table-content'>{moment(order?.date).format('YYYY-MM-DD HH:mm:ss')}</div>
           </div>
 
           <div className='order-detail-items' css={orderDetailItemsStyle}>
